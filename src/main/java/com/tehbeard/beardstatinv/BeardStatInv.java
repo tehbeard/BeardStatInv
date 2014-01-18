2b422258520f0b8b7bba4d14be4c0430507049fc
James <beardyjames@live.co.uk>
2014-01-18 00:50:45 +0000

    Initial commit
    This plugin is designed to create a single document per player,
    Addressable under domain "bsinv", key "inventory". This document should
    only be accessed via the web api, and has a structure similar to the NBT
    format used to store inventories. Some nbt data is missing (notably
    firework metadata among others).
    
    The aim is to test and release this alongside the beta of 0.8 with a
    suitable web api to display this information, as a showcase of how to
    use documents.

---
+++ src/main/java/com/tehbeard/beardstatinv/BeardStatInv.java
@@ -0,0 +1,43 @@
+package com.tehbeard.beardstatinv;
+
+import net.dragonzone.promise.Delegate;
+import net.dragonzone.promise.Promise;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.tehbeard.beardstat.BeardStat;
+import com.tehbeard.beardstat.containers.EntityStatBlob;
+import com.tehbeard.beardstat.containers.documents.DocumentRegistry;
+import com.tehbeard.beardstat.manager.EntityStatManager;
+
+public class BeardStatInv extends JavaPlugin implements Listener {
+    
+    private EntityStatManager manager;
+
+
+    @Override
+    public void onEnable() {
+        DocumentRegistry.registerDocument(InventoryDocument.class);
+        getServer().getPluginManager().registerEvents(this, this);
+        manager = getPlugin(BeardStat.class).getStatManager();
+    }
+    
+    
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event){
+        /**
+         * Create and/or load the inventory document.
+         */
+        manager.getBlobForPlayerAsync(event.getPlayer()).onResolve(new Delegate<Void, Promise<EntityStatBlob>>() {
+            
+            public <P extends Promise<EntityStatBlob>> Void invoke(P params) {
+                params.getValue().getDocument("bsinv", "inventory", InventoryDocument.class).getDocument();
+                return null;
+            }
+        });
+    }
+  
+}
---
+++ src/main/java/com/tehbeard/beardstatinv/InventoryDocument.java
@@ -0,0 +1,75 @@
+package com.tehbeard.beardstatinv;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import com.google.gson.annotations.Expose;
+import com.tehbeard.beardstat.containers.EntityStatBlob;
+import com.tehbeard.beardstat.containers.documents.IStatDocument;
+import com.tehbeard.beardstat.containers.documents.IStatDynamicDocument;
+import com.tehbeard.beardstat.containers.documents.StatDocument;
+import com.tehbeard.beardstat.containers.documents.docfile.DocumentFile;
+import com.tehbeard.beardstat.dataproviders.IStatDataProvider;
+
+@StatDocument(value="inventory",singleInstance=true)
+public class InventoryDocument implements IStatDynamicDocument{
+
+    @Expose
+    private List<JsonItem> inventory = new ArrayList<JsonItem>();
+
+    @Expose
+    private JsonItem helmet = null;
+
+    @Expose
+    private JsonItem chestplate = null;
+
+    @Expose
+    private JsonItem leggings = null;
+
+    @Expose
+    private JsonItem boots = null;
+
+    public IStatDocument mergeDocument(DocumentFile file) {
+        // Overwrite document
+        return this;
+    }
+
+    public void updateDocument(EntityStatBlob blob) {
+        if(!Bukkit.isPrimaryThread()){
+            if(!blob.getType().equals(IStatDataProvider.PLAYER_TYPE)){
+                throw new IllegalStateException("Inventory document attached to a non player object [" + blob.getName() + " : " + blob.getType());
+            }
+            Player p = Bukkit.getPlayerExact(blob.getName());
+            if(p != null){
+                //Update player inventory
+                ItemStack[] inv = p.getInventory().getContents();
+                for(int i = 0;i<inv.length;i++){
+                    if(inv[i]!=null){
+                        inventory.add(new JsonItem(i, inv[i]));
+                    }
+                }
+                if(p.getInventory().getHelmet()!=null){
+                    helmet = new JsonItem(-1,p.getInventory().getHelmet());
+                }
+
+                if(p.getInventory().getChestplate()!=null){
+                    helmet = new JsonItem(-1,p.getInventory().getChestplate());
+                }
+
+                if(p.getInventory().getLeggings()!=null){
+                    helmet = new JsonItem(-1,p.getInventory().getLeggings());
+                }
+
+                if(p.getInventory().getBoots()!=null){
+                    helmet = new JsonItem(-1,p.getInventory().getBoots());
+                }
+            }
+        }
+
+    }
+
+}
